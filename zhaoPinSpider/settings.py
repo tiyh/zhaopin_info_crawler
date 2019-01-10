808bb6cf9934976306e1793d24cc2a9e8b7beae2
tiyh <actiyh@163.com>
2019-01-10 23:39:05 +0800

    increase CONCURRENT_REQUESTS

--- zhaoPinSpider/settings.py
+++ zhaoPinSpider/settings.py
@@ -27,15 +27,15 @@ DOWNLOADER_MIDDLEWARES = {
 }
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 64
+CONCURRENT_REQUESTS = 100
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 32
-CONCURRENT_REQUESTS_PER_IP = 32
+CONCURRENT_REQUESTS_PER_DOMAIN = 100
+CONCURRENT_REQUESTS_PER_IP = 100
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
